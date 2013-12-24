22dafb1f546834cb2426829911ad5906927e9729
simplestbest <simplestbest@gmail.com>
2013-12-24 16:16:19 +0800

    Initial version.

---
+++ HelloWorld2013/HelloWorld2013.cpp
@@ -0,0 +1,12 @@
+// HelloWorld2013.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	printf("hello world visual studio 2013\n");
+	return 0;
+}
+
